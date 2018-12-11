d65061b66d7c8f128d3f2e4d4b6436e77b84c57c
Alexander Stolyarov <a100rv@gmail.com>
2018-12-11 19:49:01 +0300

    Loading screen

--- web_env.sh
+++ web_env.sh
@@ -1,6 +1,6 @@
 source ~/_null/emsdk/emsdk_env.sh 
 mkdir -p build-em
-cp match-mountain/web/index.html build-em 
-cp match-mountain/web/*.sh build-em
+cp -f match-mountain/web/index.html build-em 
+cp -f match-mountain/web/*.sh build-em
 cd build-em
 
